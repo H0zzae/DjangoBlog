8a330a49dc1206dd2a74d1a12722becb0c6ef194
H0zzae <39540561+H0zzae@users.noreply.github.com>
2020-08-03 01:25:30 +0900

    DB table Model Define

--- venvproject/h0zzae/home/admin.py
+++ venvproject/h0zzae/home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Post, Reply
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Reply)
