220bb5ea25ae9beaae386007060608a5247e5fdc
H0zzae <39540561+H0zzae@users.noreply.github.com>
2020-08-19 02:25:43 +0900

    댓글 작성폼 만듬 #3
    댓글 작성폼은 만들었지만 ~ 아직 작성하기 누르면 오류남 ^^,,,, 게시글 번호 받아서 넘기는거에서 오류나는거 같음. 흠

--- venvproject/h0zzae/home/forms.py
+++ venvproject/h0zzae/home/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
-from .models import Post
+from .models import Post,Reply
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content','attach']
+
+class ReplyForm(ModelForm):
+    class Meta:
+        model = Reply
+        fields = ['userName','content','password']
--- venvproject/h0zzae/home/views.py
+++ venvproject/h0zzae/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect,get_object_or_404, HttpResponse
 from .models import *
-from .forms import PostForm
+from .forms import PostForm,ReplyForm
 
 # Create your views here.
 def main(request):
@@ -20,13 +20,33 @@ def write(request):
         form = PostForm(request.POST or None, request.FILES or None)
         if form.is_valid():
             form.save()
-            print("hihi")
         return redirect('/post')
     else:
         form = PostForm()
     return render(request, 'post/writePost.html', {'form':form})
 
+def writeReply(request,post_id):
+    post = get_object_or_404(Post, pk=post_id)
+
+    if request.method=='POST':
+        form = ReplyForm(request.POST)
+        form.instance.userName_id = request.user.id
+        form.instance.post_id = post_id
+        if form.is_valid():
+            form = form.save()
+    form = ReplyForm()
+    replys = Reply.objects.all()
+    return render(request,'post/detail.html',{'form':form})
+
 def getPost(request,post_id):
     post_detail = get_object_or_404(Post, pk=post_id)
 
-    return render(request,'post/detail.html',{'post':post_detail})
+    if request.method=='POST':
+        form = ReplyForm(request.POST)
+        form.instance.userName_id = request.user.id
+        form.instance.post_id = post_id
+        if form.is_valid():
+            form = form.save()
+    form = ReplyForm()
+    replys = Reply.objects.all()
+    return render(request,'post/detail.html',{'post':post_detail,'replys':replys,'reply_form':form})
