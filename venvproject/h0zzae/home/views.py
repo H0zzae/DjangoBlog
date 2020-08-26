30839d7e64463f872379f445bfcfa87114955b0f
H0zzae <39540561+H0zzae@users.noreply.github.com>
2020-08-27 00:10:05 +0900

    글 작성시 카테고리랑 연결#2
    연결완료 !!

--- venvproject/h0zzae/home/views.py
+++ venvproject/h0zzae/home/views.py
@@ -21,12 +21,12 @@ def write(request):
         post = Post()
         form = PostForm(request.POST or None, request.FILES or None)
         # form.instance.category_num_id = category_id
-
         if form.is_valid():
             # post.category_num = form.category_num
             post.title = request.POST['title']
             post.content = request.POST['content']
             post.created_at = timezone.datetime.now()
+            post.category_num = request.POST['categorys']
             post.save()
             for img in request.FILES.getlist('images'):
                 photo = Photo()
